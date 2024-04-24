81a86cb47cd065d5ef15445eb74f4bba056f4089
Nguyen Long <longnguyenhoang.ict@gmail.com>
2024-04-24 16:24:43 +0700

    update load pretrained

--- trainers/base_trainer.py
+++ trainers/base_trainer.py
@@ -15,10 +15,13 @@ import random
 
 logger = setup_logger()
 
+
 class BaseTrainer:
     def __init__(self, config):
 
-        self.checkpoint_path = os.path.join(config.TRAINING.CHECKPOINT_PATH, config.MODEL.NAME)
+        self.checkpoint_path = os.path.join(
+            config.TRAINING.CHECKPOINT_PATH, config.MODEL.NAME
+        )
         if not os.path.isdir(self.checkpoint_path):
             logger.info("Creating checkpoint path")
             os.makedirs(self.checkpoint_path)
@@ -26,57 +29,72 @@ class BaseTrainer:
         if not os.path.isfile(os.path.join(self.checkpoint_path, "vocab.bin")):
             logger.info("Creating vocab")
             self.vocab = self.load_vocab(config)
-            logger.info("Saving vocab to %s" % os.path.join(self.checkpoint_path, "vocab.bin"))
-            pickle.dump(self.vocab, open(os.path.join(self.checkpoint_path, "vocab.bin"), "wb"))
+            logger.info(
+                "Saving vocab to %s" % os.path.join(self.checkpoint_path, "vocab.bin")
+            )
+            pickle.dump(
+                self.vocab, open(os.path.join(self.checkpoint_path, "vocab.bin"), "wb")
+            )
         else:
-            logger.info("Loading vocab from %s" % os.path.join(self.checkpoint_path, "vocab.bin"))
-            self.vocab = pickle.load(open(os.path.join(self.checkpoint_path, "vocab.bin"), "rb"))
+            logger.info(
+                "Loading vocab from %s"
+                % os.path.join(self.checkpoint_path, "vocab.bin")
+            )
+            self.vocab = pickle.load(
+                open(os.path.join(self.checkpoint_path, "vocab.bin"), "rb")
+            )
 
         logger.info("Loading data")
-        self.train_dataset, self.dev_dataset, self.test_dataset = self.load_feature_datasets(config.DATASET)
-        self.train_dict_dataset, self.dev_dict_dataset, self.test_dict_dataset = self.load_dict_datasets(config.DATASET)
-        
+        self.train_dataset, self.dev_dataset, self.test_dataset = (
+            self.load_feature_datasets(config.DATASET)
+        )
+        self.train_dict_dataset, self.dev_dict_dataset, self.test_dict_dataset = (
+            self.load_dict_datasets(config.DATASET)
+        )
+
         # creating iterable-dataset data loader
         self.train_dataloader = DataLoader(
             dataset=self.train_dataset,
             batch_size=config.DATASET.FEATURE_BATCH_SIZE,
             shuffle=True,
             num_workers=config.DATASET.WORKERS,
-            collate_fn=collate_fn
+            collate_fn=collate_fn,
         )
         self.val_dataloader = DataLoader(
             dataset=self.dev_dataset,
             batch_size=config.DATASET.FEATURE_BATCH_SIZE,
             shuffle=True,
             num_workers=config.DATASET.WORKERS,
-            collate_fn=collate_fn
+            collate_fn=collate_fn,
         )
         self.test_dataloader = DataLoader(
             dataset=self.test_dataset,
             batch_size=config.DATASET.FEATURE_BATCH_SIZE,
             shuffle=True,
             num_workers=config.DATASET.WORKERS,
-            collate_fn=collate_fn
+            collate_fn=collate_fn,
         )
 
         # creating dictionary iterable-dataset data loader
         self.train_dict_dataloader = DataLoader(
             dataset=self.train_dict_dataset,
-            batch_size=config.DATASET.DICT_BATCH_SIZE // config.TRAINING.TRAINING_BEAM_SIZE,
+            batch_size=config.DATASET.DICT_BATCH_SIZE
+            // config.TRAINING.TRAINING_BEAM_SIZE,
             shuffle=True,
-            collate_fn=collate_fn
+            collate_fn=collate_fn,
         )
         self.val_dict_dataloader = DataLoader(
             dataset=self.dev_dict_dataset,
-            batch_size=config.DATASET.DICT_BATCH_SIZE // config.TRAINING.EVALUATING_BEAM_SIZE,
+            batch_size=config.DATASET.DICT_BATCH_SIZE
+            // config.TRAINING.EVALUATING_BEAM_SIZE,
             shuffle=True,
-            collate_fn=collate_fn
+            collate_fn=collate_fn,
         )
         self.test_dict_dataloader = DataLoader(
             dataset=self.test_dict_dataset,
             batch_size=1,
             shuffle=True,
-            collate_fn=collate_fn
+            collate_fn=collate_fn,
         )
 
         logger.info("Building model")
@@ -86,7 +104,9 @@ class BaseTrainer:
 
         logger.info("Defining optimizer and objective function")
         self.configuring_hyperparameters(config)
-        self.optim = Adam(self.model.parameters(), lr=config.TRAINING.LEARNING_RATE, betas=(0.9, 0.98))
+        self.optim = Adam(
+            self.model.parameters(), lr=config.TRAINING.LEARNING_RATE, betas=(0.9, 0.98)
+        )
         self.scheduler = LambdaLR(self.optim, self.lambda_lr)
         self.loss_fn = NLLLoss(ignore_index=self.vocab.padding_idx)
 
@@ -114,7 +134,9 @@ class BaseTrainer:
     def lambda_lr(self, step):
         warm_up = self.warmup
         step += 1
-        return (self.model.encoder.d_model ** -.5) * min(step ** -.5, step * warm_up ** -1.5)
+        return (self.model.encoder.d_model**-0.5) * min(
+            step**-0.5, step * warm_up**-1.5
+        )
 
     def load_checkpoint(self, fname) -> dict:
         if not os.path.exists(fname):
@@ -122,35 +144,37 @@ class BaseTrainer:
 
         logger.info("Loading checkpoint from %s", fname)
 
-        checkpoint = torch.load(fname)
+        checkpoint = torch.load(fname, map_location=torch.device("cpu"))
 
-        torch.set_rng_state(checkpoint['torch_rng_state'])
-        torch.cuda.set_rng_state(checkpoint['cuda_rng_state'])
-        np.random.set_state(checkpoint['numpy_rng_state'])
-        random.setstate(checkpoint['random_rng_state'])
+        torch.set_rng_state(checkpoint["torch_rng_state"])
+        torch.cuda.set_rng_state(checkpoint["cuda_rng_state"])
+        np.random.set_state(checkpoint["numpy_rng_state"])
+        random.setstate(checkpoint["random_rng_state"])
 
-        self.model.load_state_dict(checkpoint['state_dict'], strict=False)
+        self.model.load_state_dict(checkpoint["state_dict"], strict=False)
 
-        logger.info(f"Resuming from epoch %s", checkpoint['epoch'])
+        logger.info(f"Resuming from epoch %s", checkpoint["epoch"])
 
         return checkpoint
 
     def save_checkpoint(self, dict_for_updating: dict) -> None:
         dict_for_saving = {
-            'torch_rng_state': torch.get_rng_state(),
-            'cuda_rng_state': torch.cuda.get_rng_state(),
-            'numpy_rng_state': np.random.get_state(),
-            'random_rng_state': random.getstate(),
-            'epoch': self.epoch,
-            'state_dict': self.model.state_dict(),
-            'optimizer': self.optim.state_dict(),
-            'scheduler': self.scheduler.state_dict()
+            "torch_rng_state": torch.get_rng_state(),
+            "cuda_rng_state": torch.cuda.get_rng_state(),
+            "numpy_rng_state": np.random.get_state(),
+            "random_rng_state": random.getstate(),
+            "epoch": self.epoch,
+            "state_dict": self.model.state_dict(),
+            "optimizer": self.optim.state_dict(),
+            "scheduler": self.scheduler.state_dict(),
         }
 
         for key, value in dict_for_updating.items():
             dict_for_saving[key] = value
 
-        torch.save(dict_for_saving, os.path.join(self.checkpoint_path, "last_model.pth"))
+        torch.save(
+            dict_for_saving, os.path.join(self.checkpoint_path, "last_model.pth")
+        )
 
     def start(self):
         raise NotImplementedError
